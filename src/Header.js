073f6170412c9c196cb69e49f88e1b2dc0d9d879
ahmedhossam <hoss4614@gmail.com>
2023-02-03 19:18:24 +0200

    add a background and finish initial styling for header

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="header-name">FOOTBALL NFT</h1>
+      <button>Shop</button>
+      <button>Cart</button>
+    </header>
+  );
+};
+
+export default Header;
