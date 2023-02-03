0778a09f68bebac0c7561e10219ec5405ba44027
ahmedhossam <hoss4614@gmail.com>
2023-02-03 19:53:34 +0200

    finish header styling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import './App.css';
 import Header from './Header';
 
 function App() {
-  return <Header />;
+  return (
+    <div className="main">
+      <Header />
+      <div className="home">
+        <button className="visit-shop-button">Visit The Shop</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -4,8 +4,10 @@ const Header = () => {
   return (
     <header className="header">
       <h1 className="header-name">FOOTBALL NFT</h1>
-      <button>Shop</button>
-      <button>Cart</button>
+      <div className="header-buttons">
+        <button>Shop</button>
+        <button>Cart</button>
+      </div>
     </header>
   );
 };
