857199e166bff3031ae8cc256ec6925dbf6a3d92
ahmedhossam <hoss4614@gmail.com>
2023-02-03 20:06:47 +0200

    finish initial routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 import Header from './Header';
 import VisitShopButton from './VisitShopButton';
+import Shop from './Shop';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="main">
-      <Header />
-      <VisitShopButton />
-    </div>
+    <BrowserRouter>
+      <div className="main">
+        <Header />
+        <Routes>
+          <Route path="/" element={<VisitShopButton />} />
+          <Route path="/shop" element={<Shop />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
